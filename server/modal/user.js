c1c9f06e47c62d5568c3f36856b39e9283de1f29
Haroon Khan <HaroonKhan120704@gmail.com>
2023-10-15 11:05:34 +0100

    created user and auth for if user exists

---
+++ server/modal/user.js
@@ -0,0 +1,44 @@
+const mysql = require('mysql2');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'chat-app',
+});
+const createUser = async (username, password) => {
+  conn.query('INSERT INTO users (username, password) VALUES (?,?)', [
+    username,
+    password,
+  ]);
+};
+const hashPassword = async (password, saltRounds) => {
+  const res = bcrypt.hashSync(password, saltRounds);
+  return res;
+};
+const isUserExists = async (username) => {
+  const res = conn
+    .promise()
+    .query('SELECT * FROM users WHERE username = ?', [username])
+    .then(([rows, fields]) => {
+      if (rows.length > 0) {
+        return rows[0];
+      } else {
+        return false;
+      }
+    });
+  return res;
+};
+async function comparePassswords(passwords, hash) {
+  return bcrypt.compareSync(passwords, hash);
+}
+
+module.exports = {
+  createUser,
+  hashPassword,
+  isUserExists,
+  comparePassswords,
+};
