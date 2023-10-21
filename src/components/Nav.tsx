86dc3df563d666939c64b5478d235dfc76b39c14
Haroon Khan <HaroonKhan120704@gmail.com>
2023-10-21 12:28:45 +0100

    added styles for nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,11 +2,19 @@ const Nav = ({ clientUsername, logOut, deleteAccount }: any) => {
   return (
     <nav>
       <ul className='flex flex-row justify-end'>
-        <li className='text-white m-3'>Account: {clientUsername}</li>
-        <li className='text-white m-3 cursor-pointer' onClick={logOut}>
+        <li className='text-neutral-800 m-3 font-mono font-bold'>
+          Account: {clientUsername}
+        </li>
+        <li
+          className='text-neutral-800 m-3 font-mono font-bold cursor-pointer'
+          onClick={logOut}
+        >
           Log Out
         </li>
-        <li className='text-white m-3 cursor-pointer' onClick={deleteAccount}>
+        <li
+          className='text-neutral-800 m-3 font-mono font-bold cursor-pointer'
+          onClick={deleteAccount}
+        >
           Delete Account
         </li>
       </ul>
