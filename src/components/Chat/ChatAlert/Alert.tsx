0fa745533927c5783f3311fc90b04f0c9af00da5
Haroon Khan <HaroonKhan120704@gmail.com>
2023-10-24 00:01:46 +0100

    created alert

--- src/components/Chat/ChatAlert/Alert.tsx
+++ src/components/Chat/ChatAlert/Alert.tsx
@@ -1,5 +1,15 @@
-const Alert = () => {
-  return <div></div>;
+const Alert = ({ modalMessage, closeModal }: any) => {
+  setTimeout(() => {
+    closeModal();
+  }, 3000);
+
+  return (
+    <div className='fixed top-0 left-0 w-full mt-5 flex items-center justify-center'>
+      <div className='w-96 bg-red-500 text-white p-4 flex items-center justify-center rounded-xl'>
+        <p className='text-center text-xl font-bold'>{modalMessage}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Alert;
